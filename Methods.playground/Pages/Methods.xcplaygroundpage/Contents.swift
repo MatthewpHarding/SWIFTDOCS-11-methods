26a7a083da4b6cb90d525696ecb8cf939959b78a
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 14:59:05 +0100

    Resolved compilation errors within the Xcode playground

--- Methods.playground/Pages/Methods.xcplaygroundpage/Contents.swift
+++ Methods.playground/Pages/Methods.xcplaygroundpage/Contents.swift
@@ -52,8 +52,11 @@ counter.reset()
 //: Every instance of a type has an implicit property called self, which is exactly equivalent to the instance itself. You use the self property to refer to the current instance within its own instance methods.
 //:
 //: The increment() method in the example above could have been written like this:
-func increment() {
-    self.count += 1
+class Counter2 {
+    var count = 0
+    func increment() {
+        self.count += 1
+    }
 }
 //: In practice, you don’t need to write self in your code very often. If you don’t explicitly write self, Swift assumes that you are referring to a property or method of the current instance whenever you use a known property or method name within a method. This assumption is demonstrated by the use of count (rather than self.count) inside the three instance methods for Counter.
 //:
@@ -80,30 +83,31 @@ if somePoint.isToTheRightOf(x: 1.0) {
 //: However, if you need to modify the properties of your structure or enumeration within a particular method, you can opt in to mutating behavior for that method. The method can then mutate (that is, change) its properties from within the method, and any changes that it makes are written back to the original structure when the method ends. The method can also assign a completely new instance to its implicit self property, and this new instance will replace the existing one when the method ends.
 //:
 //: You can opt in to this behavior by placing the mutating keyword before the func keyword for that method:
-struct Point {
+struct Point2 {
     var x = 0.0, y = 0.0
     mutating func moveBy(x deltaX: Double, y deltaY: Double) {
         x += deltaX
         y += deltaY
     }
 }
-var somePoint = Point(x: 1.0, y: 1.0)
-somePoint.moveBy(x: 2.0, y: 3.0)
-print("The point is now at (\(somePoint.x), \(somePoint.y))")
+var somePoint2 = Point2(x: 1.0, y: 1.0)
+somePoint2.moveBy(x: 2.0, y: 3.0)
+print("The point is now at (\(somePoint2.x), \(somePoint2.y))")
 // Prints "The point is now at (3.0, 4.0)"
 //: The Point structure above defines a mutating moveBy(x:y:) method, which moves a Point instance by a certain amount. Instead of returning a new point, this method actually modifies the point on which it’s called. The mutating keyword is added to its definition to enable it to modify its properties.
 //:
 //: Note that you can’t call a mutating method on a constant of structure type, because its properties can’t be changed, even if they’re variable properties, as described in Stored Properties of Constant Structure Instances:
-let fixedPoint = Point(x: 3.0, y: 3.0)
-fixedPoint.moveBy(x: 2.0, y: 3.0)
+// ⛔️ Compilation error: uncomment to view
+//let fixedPoint = Point2(x: 3.0, y: 3.0)
+//fixedPoint.moveBy(x: 2.0, y: 3.0)
 // this will report an error
 //: ### Assigning to self Within a Mutating Method
 //:
 //: Mutating methods can assign an entirely new instance to the implicit self property. The Point example shown above could have been written in the following way instead:
-struct Point {
+struct Point3 {
     var x = 0.0, y = 0.0
     mutating func moveBy(x deltaX: Double, y deltaY: Double) {
-        self = Point(x: x + deltaX, y: y + deltaY)
+        self = Point3(x: x + deltaX, y: y + deltaY)
     }
 }
 //: This version of the mutating moveBy(x:y:) method creates a new structure whose x and y values are set to the target location. The end result of calling this alternative version of the method will be exactly the same as for calling the earlier version.
