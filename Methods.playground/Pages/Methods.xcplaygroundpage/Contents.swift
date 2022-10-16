544706343df8650e8626b786234d9adcc089df1f
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 11:08:33 +0100

    Added the page number (from official Swift documentation) to each Xcode playground file.

--- Methods.playground/Pages/Methods.xcplaygroundpage/Contents.swift
+++ Methods.playground/Pages/Methods.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: ![Swift](swift-logo.png)
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/Methods.html)
-//: # Methods
+//: # 11. Methods
 //:
 //:
 //: Methods are functions that are associated with a particular type. Classes, structures, and enumerations can all define instance methods, which encapsulate specific tasks and functionality for working with an instance of a given type. Classes, structures, and enumerations can also define type methods, which are associated with the type itself. Type methods are similar to class methods in Objective-C.
